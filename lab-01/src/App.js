cc87af1acc6f7411388928e6558fccd71377ddfd
Prayag <tandonprayag99@gmail.com>
2024-09-21 10:59:05 -0400

    Final commit for Lab-01

--- lab-01/src/App.js
+++ lab-01/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Greeter from './Greeter';
 
 function App() {
   return (
     <div>
-      <h1>Hello, React!</h1>
+      <h1>Welcome to my React App</h1>
+      <Greeter />
     </div>
   );
 }
