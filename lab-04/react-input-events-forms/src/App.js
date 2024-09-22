1e34f60e76663ba9d6b046cadbf08a71d0ee7e91
Prayag <tandonprayag99@gmail.com>
2024-09-22 13:04:34 -0400

    Setting up the form

--- lab-04/react-input-events-forms/src/App.js
+++ lab-04/react-input-events-forms/src/App.js
@@ -2,27 +2,75 @@ import React, { useState } from 'react';
 
 function App() {
   const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
 
   // FUNTION TO HANDLE INPUT CHANGE
   const handleInputChange = (e) => {
-    setName(e.target.value);
-  }
+    const { name, value } = e.target;
+    if (name === 'name') {
+      setName(value);
+    } else if (name === 'email') {
+      setEmail(value);
+    }
+  };
+
+  // FUNCTION TO PREVEN FORMS DEFAULT BEHAVIOUR
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Name: ${name}\nEmail: ${email}`);
+  };
+
+  // FUNCTION TO RESET VALUES
+
 
   return (
     <div className='App'>
       <h1 style={{ textAlign: 'center', textDecoration: 'underline', marginBottom: '32px' }}>React Input Events and Forms</h1>
-      <input
-        type='text'
-        placeholder='Enter your name'
-        style={{
-          padding: '6px',
-          borderRadius: '4px',
-          marginInline: '16px'
-        }}
-        value={name}
-        onChange={handleInputChange}
-      />
-      <p style={{ fontSize: '16px', fontWeight: 'bold' }}> Hello, {name}</p>
+      <form onSubmit={handleSubmit}>
+        {/* NAME INPUT FIELD */}
+        <label>
+          Name:
+          <input
+            type='text'
+            name='name'
+            placeholder='Enter your name'
+            style={{
+              padding: '6px',
+              borderRadius: '4px',
+              margin: '16px'
+            }}
+            value={name}
+            onChange={handleInputChange}
+          />
+        </label>
+        {/* EMAIL INPUT FIELD */}
+        <label>
+          Email:
+          <input
+            type='email'
+            name='email'
+            placeholder='Enter your name'
+            style={{
+              padding: '6px',
+              borderRadius: '4px',
+              marginInline: '16px'
+            }}
+            value={email}
+            onChange={handleInputChange}
+          />
+        </label>
+        <button
+          type='submit'
+          style={{
+            display: 'inline-block',
+            padding: '6px',
+            borderRadius: '6px',
+            backgroundColor: 'greenyellow'
+          }}
+        >Submit</button>
+      </form>
+      <p style={{ fontSize: '16px', fontWeight: 'bold' }}> Name: {name}</p>
+      <p style={{ fontSize: '16px', fontWeight: 'bold' }}> Email: {email}</p>
     </div>
   );
 }
