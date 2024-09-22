71066c3b5baf6b301267bb585e6d350798b8292e
Prayag <tandonprayag99@gmail.com>
2024-09-22 11:07:58 -0400

    Lab-03 intermediate complete

--- lab-03/intermediate-react-hooks/src/Components/Todo.js
+++ lab-03/intermediate-react-hooks/src/Components/Todo.js
@@ -6,16 +6,16 @@ function Todo() {
 
     // FUNCTION TO ADD NEW ITEM TO THE TODO LIST
     const addTodo = () => {
-        if (newTodo.trim === '') return; //Guard Clause
+        if (newTodo.trim() === '') return; //Guard Clause
         setTodos([...todos, newTodo]);
         setNewTodo('') //Clearing the input field
     };
 
     // FUNCTION TO REMOVE AN ITEM TO THE TODO LIST
     const removeTodo = (index) => {
-        const updateTodos = [...todos];
-        updateTodos.splice(index, 1);
-        setTodos(updateTodos);
+        const updatedTodos = [...todos];
+        updatedTodos.splice(index, 1);
+        setTodos(updatedTodos);
     }
 
     return (
@@ -28,19 +28,25 @@ function Todo() {
                 placeholder='Add a new todo'
                 value={newTodo}
                 onChange={(e) => setNewTodo(e.target.value)}
+                style={{
+                    display: 'inline-block',
+                    padding: '8px',
+                    marginInline: '12px',
+                    borderRadius: '6px',
+                }}
             />
             <button
-                style={{ padding: '10px', borderRadius: '8px', backgroundColor: 'darkblue' }}
+                style={{ padding: '10px', borderRadius: '8px', backgroundColor: 'darkblue', color: 'whitesmoke', cursor: 'pointer' }}
                 onClick={addTodo}>Add New todo</button>
 
             {/* TODO LIST */}
             <ul>
-                {todos.map((todo, index) => {
+                {todos.map((todo, index) => (
                     <li key={index}>
                         {todo}
-                        <button style={{ padding: '10px', borderRadius: '8px', backgroundColor: 'red', color: 'white' }} onClick={() => removeTodo(index)}>Remove</button>
+                        <button style={{ padding: '4px', borderRadius: '8px', backgroundColor: 'red', color: 'white', marginLeft: '16px', cursor: 'pointer' }} onClick={() => removeTodo(index)}>Remove</button>
                     </li>
-                })}
+                ))}
             </ul>
         </div>
     );
