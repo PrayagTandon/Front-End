3b03a23b5449a0882fc1477ce86ed0c7b84bb145
Prayag <tandonprayag99@gmail.com>
2024-09-22 22:47:14 -0400

    Lab-05 complete

--- lab-05/react-router-tutorial/src/App.js
+++ lab-05/react-router-tutorial/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
+// function userProfile({ match }) {
+//   const userId = match.param.id;
+
+//   return (
+//     <div>
+//       <h2>User Profile</h2>
+//       <p>User ID: {userId}</p>
+//     </div>
+//   );
+// }
+
+function Dashboard() {
+  return <h2>Dashboard</h2>;
+}
+
+function Login() {
+  return <h2>Login</h2>;
+}
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/dashboard">
+          {loggedIn ? <Dashboard /> : <Redirect to="/login" />}
+        </Route>
+        <Route path="/login" component={Login} />
+        <Redirect from='/' to="/dashboard" />
+      </Switch>
+    </Router>
   );
 }
 
