324ac06757b047e16b8d9dc7952184d560d648ef
Prayag <tandonprayag99@gmail.com>
2024-10-02 11:08:19 -0400

    ...

--- ethereum-blockchain-explorer/src/App.jsx
+++ ethereum-blockchain-explorer/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import Dashboard from './components/Dashboard';
 import Transactions from './components/Transactions';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <Transactions />
+    <div className="min-h-screen bg-gray-100">
+      <Navbar />
+      <div className="p-6">
+        <Dashboard />
+        <Transactions />
+      </div>
     </div>
   );
 }
