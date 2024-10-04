a1fac6460e6913db068e1b53ee148e8cc03a1c63
Prayag <tandonprayag99@gmail.com>
2024-10-04 01:27:23 -0400

    FInalising on the style

--- ethereum-blockchain-explorer/src/components/Navbar.jsx
+++ ethereum-blockchain-explorer/src/components/Navbar.jsx
@@ -4,13 +4,13 @@ import MenuIcon from '@mui/icons-material/Menu';
 
 const Navbar = () => {
     return (
-        <AppBar position="static" className="bg-black">
+        <AppBar position="static" sx={{ backgroundColor: 'rgb(67 20 7)' }}>
             <Toolbar>
                 <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
                     <MenuIcon />
                 </IconButton>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    Blockchain Explorer
+                    Ethereum Blockchain Explorer
                 </Typography>
             </Toolbar>
         </AppBar>
